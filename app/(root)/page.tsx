8f9058241e3f84e078f9565400754e314c7ee589
mtang34705 <mtang34705@yahoo.com>
2026-05-01 17:05:54 +0100

    37:06

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import Hello from "@/components/hello"
+
+
+const Home = () => {
+  console.log('???')
+  return (
+    <main>
+    <div className='text-5xl'>Welcome to Next.js</div>
+    <Hello/>
+    </main>
+  )
+}
+
+export default Home
